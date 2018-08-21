1442850d924601a7b78a39f0679ce355ce09782f
石亀広大 <kira5052@gmail.com>
2018-08-22 02:38:51 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+	el: "#app",
+	data() {
+		return {
+			message: 'App start'
+		}
+	}
+});
--- webpack/prod.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const baseConfig = require('./base.config.js');
-let merge = require('webpack-merge');
-
-const config = merge(baseConfig, {
-	mode: 'production',
-});
-
-module.exports = config;
